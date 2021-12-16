1c4b777799d1f9a7776e450365a3e601c0640b08
MukanovaAidai <95532639+MukanovaAidai@users.noreply.github.com>
2021-12-16 15:10:29 +0700

    vairiable

---
+++ vairiable/vairiable.js
@@ -0,0 +1,13 @@
+let min = 10;
+if ((min >= 0) && (min <= 14)) {
+    alert('1');
+}
+if ((min >= 15) && (min <= 30)) {
+    alert('2');
+}
+if ((min >= 31) && (min <= 45)) {
+    alert('3');
+}
+if ((min >= 46) && (min <= 59)) {
+    alert('4');
+}
